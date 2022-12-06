dc5fc40f70748c8b15c7705d868e07dc4e13810a
Mitch Rosenburg <mitch@inksoft.com>
2022-12-06 06:19:47 -0800

    Clean up DayTestAttribute.

--- AdventOfCode2022.Tests/DayTests.cs
+++ AdventOfCode2022.Tests/DayTests.cs
@@ -8,12 +8,7 @@ namespace AdventOfCode2022.Tests;
 public class DayTests
 {
 	[Theory]
-	[DayTestCases(1)]
-	[DayTestCases(2)]
-	[DayTestCases(3)]
-	[DayTestCases(4)]
-	[DayTestCases(5)]
-	[DayTestCases(6)]
+	[DayTestCases(1, 2, 3, 4, 5, 6)]
 	public void AllDays(IDay day, string input, string expectedOutput)
 	{
 		var inputLines = input.Split(Environment.NewLine);
@@ -24,39 +19,41 @@ public class DayTests
 
 public class DayTestCasesAttribute : DataAttribute
 {
-	public DayTestCasesAttribute(int day)
+	public DayTestCasesAttribute(params int[] days)
 	{
-		m_day = day;
+		m_dayNumbers = days;
 	}
 
 	public override IEnumerable<object[]> GetData(MethodInfo testMethod)
 	{
-		string dayTypeName = $"Day{m_day:D2}";
-
-		string testCasesFolder = Path.Combine(Environment.CurrentDirectory, $"TestCases/{dayTypeName}");
-		if (!Directory.Exists(testCasesFolder))
-			throw new InvalidOperationException($"Folder does not exist: {testCasesFolder}");
-
-		var dayType = typeof(IDay).Assembly.GetTypes().FirstOrDefault(x => x.Name == dayTypeName);
-		if (dayType is null)
-			throw new InvalidOperationException($"Type does not exist: {dayTypeName}");
-
-		var day = (IDay) Activator.CreateInstance(dayType);
-		foreach (var files in Directory.GetFiles(testCasesFolder, "*.txt").GroupBy(file => Path.GetFileName(file).Split('_')[0]))
+		foreach (int dayNumber in m_dayNumbers)
 		{
-			string inFileName = $"{files.Key}_in.txt";
-			string inFile = files.FirstOrDefault(x => Path.GetFileName(x).ToLower() == inFileName);
-			if (inFile is null)
-				throw new InvalidOperationException($"File does not exist: {inFileName}");
+			string dayTypeName = $"Day{dayNumber:D2}";
+			var dayType = typeof(IDay).Assembly.GetTypes().FirstOrDefault(x => x.Name == dayTypeName);
+			if (dayType is null)
+				throw new InvalidOperationException($"Type does not exist: {dayTypeName}");
 
-			string outFileName = $"{files.Key}_out.txt";
-			string outFile = files.FirstOrDefault(x => Path.GetFileName(x.ToLower()) == outFileName);
-			if (outFile is null)
-				throw new InvalidOperationException($"File does not exist: {outFileName}");
+			string testCasesFolder = Path.Combine(Environment.CurrentDirectory, $"TestCases/{dayType.Name}");
+			if (!Directory.Exists(testCasesFolder))
+				throw new InvalidOperationException($"Folder does not exist: {testCasesFolder}");
 
-			yield return new object[] { day, File.ReadAllText(inFile).TrimEnd(), File.ReadAllText(outFile).TrimEnd() };
+			var day = (IDay) Activator.CreateInstance(dayType);
+			foreach (var files in Directory.GetFiles(testCasesFolder, "*.txt").GroupBy(file => Path.GetFileName(file).Split('_')[0]))
+			{
+				string inFileName = $"{files.Key}_in.txt";
+				string inFile = files.FirstOrDefault(x => Path.GetFileName(x).ToLower() == inFileName);
+				if (inFile is null)
+					throw new InvalidOperationException($"File does not exist: {inFileName}");
+
+				string outFileName = $"{files.Key}_out.txt";
+				string outFile = files.FirstOrDefault(x => Path.GetFileName(x.ToLower()) == outFileName);
+				if (outFile is null)
+					throw new InvalidOperationException($"File does not exist: {outFileName}");
+
+				yield return new object[] { day, File.ReadAllText(inFile).TrimEnd(), File.ReadAllText(outFile).TrimEnd() };
+			}
 		}
 	}
 
-	private readonly int m_day;
+	private readonly IReadOnlyList<int> m_dayNumbers;
 }

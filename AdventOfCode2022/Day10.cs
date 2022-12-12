d9cd81d31d1bfe236f5ce68e64908a19c0d2b86c
Mitch Rosenburg <mitch@inksoft.com>
2022-12-12 14:13:01 -0800

    Day 10 cleanup.

--- AdventOfCode2022/Day10.cs
+++ AdventOfCode2022/Day10.cs
@@ -5,10 +5,9 @@ public class Day10 : IDay
 	public IReadOnlyList<string> Execute(IReadOnlyList<string> input)
 	{
 		var output = new List<string>();
-		var allCycles = Run(input).ToList();
-		output.Add(allCycles.Where(x => x.Cycle == 20 || (x.Cycle - 20) % 40 == 0).Take(6).Select(x => x.X * x.Cycle).Sum().ToString());
+		output.Add(Run(input).Where(x => x.Cycle == 20 || (x.Cycle - 20) % 40 == 0).Take(6).Sum(x => x.X * x.Cycle).ToString());
 
-		foreach (var row in allCycles.GroupBy(x => (x.Cycle - 1) / 40))
+		foreach (var row in Run(input).GroupBy(x => (x.Cycle - 1) / 40))
 		{
 			string line = "";
 			foreach (var (x, pos) in row.Select((x, i) => (x.X, i)))
@@ -25,14 +24,12 @@ public class Day10 : IDay
 		int cycle = 0;
 		foreach (var instruction in instuctions)
 		{
-			cycle++;
-			yield return (cycle, x);
+			yield return (++cycle, x);
 
 			var tokens = instruction.Split(' ');
 			if (tokens[0] == "addx")
 			{
-				cycle++;
-				yield return (cycle, x);
+				yield return (++cycle, x);
 				x += int.Parse(tokens[1]);
 			}
 		}

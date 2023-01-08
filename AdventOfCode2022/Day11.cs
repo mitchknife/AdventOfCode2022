833e01cacc86cbce0ec96b1ac72c0fce9378c5b8
Mitch Rosenburg <mitch@inksoft.com>
2023-01-08 15:01:46 -0800

    Day 11 part 2.
    Needed a little help with this one :(

--- AdventOfCode2022/Day11.cs
+++ AdventOfCode2022/Day11.cs
@@ -4,86 +4,78 @@ public class Day11 : IDay
 {
 	public IEnumerable<string> Execute(IReadOnlyList<string> input)
 	{
-		var part1Monkeys = ObserveMonkeys(input, 20, 3);
-
-		yield return part1Monkeys.OrderByDescending(x => x.TotalItemsInspected).Take(2).Select(x => x.TotalItemsInspected).Aggregate(1, (x, y) => x * y).ToString();
+		yield return GetLevelOfMonkeyBusiness(input, 20, 3).ToString();
+		yield return GetLevelOfMonkeyBusiness(input, 10000, null).ToString();
 	}
 
-	static IReadOnlyList<Monkey> ObserveMonkeys(IEnumerable<string> input, int rounds, int worryModifier)
+	private long GetLevelOfMonkeyBusiness(IReadOnlyList<string> input, int rounds, int? worryModifier)
 	{
-		var monkeys = new List<Monkey>();
-		foreach (var monkeyLines in input.Select((x, i) => (x, i)).GroupBy(x => x.i / 7, x => x.x, (key, items) => items.ToList()))
-			Monkey.ParseAndAddMonkey(monkeyLines, monkeys, worryModifier);
+		var monkeys = input
+			.Select((x, i) => (x, i))
+			.GroupBy(x => x.i / 7, x => x.x, (key, items) => items.ToList())
+			.Select(Monkey.Create)
+			.ToList();
 
 		foreach (var round in Enumerable.Range(1, rounds))
 		{
 			foreach (var monkey in monkeys)
-				monkey.InspectAllItems();
+				monkey.InspectItems(monkeys, worryModifier);
 		}
 
-		return monkeys;
+		return monkeys
+			.OrderByDescending(x => x.TotalItemsInspected)
+			.Take(2)
+			.Select(x => (long) x.TotalItemsInspected)
+			.Aggregate(1L, (x, y) => x * y);
 	}
 
-	private record Item(int WorryLevel);
+	private record Item(long WorryLevel);
+	private record Operation(string Op, int? Value);
+	private record Test(int Divisor, int TrueMonkeyId, int FalseMonkeyId);
 
-	private class Monkey
+	private record Monkey(int Id, Queue<Item> Items, Operation Operation, Test Test)
 	{
-		public static void ParseAndAddMonkey(IReadOnlyList<string> monkeyLines, IList<Monkey> monkeys, int worryModifier)
+		public static Monkey Create(IReadOnlyList<string> monkeyLines)
 		{
-			int monkeyNumber = int.Parse(monkeyLines[0].Split(' ').Last().TrimEnd(':'));
-			var initialItems = monkeyLines[1].Split(':')[1].Split(',').Select(x => new Item(int.Parse(x.Trim()))).ToList();
-			
 			var operationTokens = monkeyLines[2].Split('=')[1].Split(' ');
-			Item adjustWorryLevel(Item item)
-			{
-				int rightSide = int.TryParse(operationTokens[3], out var i) ? i : item.WorryLevel;
-
-				var next = operationTokens[2] == "*"
-					? item.WorryLevel * rightSide
-					: item.WorryLevel + rightSide;
-
-				if (worryModifier > 1)
-					next = next / worryModifier;
-
-				return new Item(next);
-			}
-
-			Monkey pickNextMonkey(Item item)
-			{
-				int divisor = int.Parse(monkeyLines[3].Split(' ').Last());
-				// BigInteger.TryParse(item.WorryLevel, out var worryLevel);
-				int monkeyIndex = int.Parse(monkeyLines[(item.WorryLevel % divisor == 0) ? 4 : 5].Split(' ').Last());
-				return monkeys[monkeyIndex];
-			}
-
-			monkeys.Add(new Monkey(monkeyNumber, initialItems, adjustWorryLevel, pickNextMonkey));
+			return new Monkey(
+				Id: int.Parse(monkeyLines[0].Split(' ').Last().TrimEnd(':')),
+				Items: new(monkeyLines[1].Split(':')[1].Split(',').Select(x => new Item(long.Parse(x.Trim())))),
+				Operation: new (
+					Op: operationTokens[2],
+					Value: int.TryParse(operationTokens[3], out var i) ? i : null
+				),
+				Test: new (
+					Divisor: int.Parse(monkeyLines[3].Split(' ').Last()),
+					TrueMonkeyId: int.Parse(monkeyLines[4].Split(' ').Last()),
+					FalseMonkeyId: int.Parse(monkeyLines[5].Split(' ').Last())
+				)
+			);
 		}
 
-		private Monkey(int number, IReadOnlyList<Item> initialItems, Func<Item, Item> adjustWorryLevel, Func<Item, Monkey> pickNextMonkey)
+		public int TotalItemsInspected { get; private set; }
+
+		public void InspectItems(IReadOnlyList<Monkey> monkeys, int? worryModifier)
 		{
-			Number = number;
-			m_items = new Queue<Item>(initialItems);
-			m_adjustWorryLevel = adjustWorryLevel;
-			m_pickNextMonkey = pickNextMonkey;
-		}
-
-		public int Number { get; }
-
-		public int TotalItemsInspected { get; private set;}
-
-		public void InspectAllItems()
-		{
-			while (m_items.TryDequeue(out Item item))
+			int mod = monkeys.Select(x => x.Test.Divisor).Aggregate(1, (x, y) => x * y);
+			while (Items.TryDequeue(out Item item))
 			{
+				long worryLevel = item.WorryLevel;
+				if (worryModifier is null)
+					worryLevel = worryLevel % mod;
+				
+				long opValue = Operation.Value ?? worryLevel;
+				worryLevel = Operation.Op == "*"
+					? worryLevel * opValue
+					: worryLevel + opValue;
+
+				if (worryModifier is not null)
+					worryLevel = worryLevel / worryModifier.Value;
+
+				var nextMonkeyId = worryLevel % Test.Divisor == 0 ? Test.TrueMonkeyId : Test.FalseMonkeyId;
+				monkeys.Single(x => x.Id == nextMonkeyId).Items.Enqueue(new Item(worryLevel));
 				TotalItemsInspected++;
-				item = m_adjustWorryLevel(item);
-				var nextMonkey = m_pickNextMonkey(item);
-				nextMonkey.m_items.Enqueue(item);
 			}
 		}
-
-		private readonly Queue<Item> m_items;
-		private readonly Func<Item, Item> m_adjustWorryLevel;
-		private readonly Func<Item, Monkey> m_pickNextMonkey;
 	}
 }
